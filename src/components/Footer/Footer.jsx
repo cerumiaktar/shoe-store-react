fecbd1e018f99241075586a893f59d93e294088c
unknown <ce.rumiakter@gmail.com>
2025-02-05 15:36:57 +0600

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,24 +5,25 @@ const Footer = () => {
         <div className="bg-neutral">
             <footer className="footer sm:footer-horizontal container mx-auto text-neutral-content p-10">
                 <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <h6 className="footer-title">Contact</h6>
+                    <a className="link link-hover">Email: info@comfortstep.com</a>
+                    <a className="link link-hover">Helpline: +8809234567890 </a>
+                    <a className="link link-hover">Trade Number: TRAD/DSCC/051175/2025</a>
+                    <a className="link link-hover">Thank You for Visiting Us</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Company</h6>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <h6 className="footer-title">Useful Links</h6>
+                    <a className="link link-hover">Privacy Policy</a>
+                    <a className="link link-hover">Terms & Condition</a>
+                    <a className="link link-hover">Refund Policy</a>
+                    <a className="link link-hover">Exchange Policy</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <h6 className="footer-title">Follow</h6>
+                    <a className="link link-hover">Facebook</a>
+                    <a className="link link-hover">Instagram</a>
+                    <a className="link link-hover">Youtube</a>
+                    <a className="link link-hover">Tiktok</a>
                 </nav>
             </footer>
         </div>
